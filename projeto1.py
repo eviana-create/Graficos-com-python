c4786f0a8c3c0bcb77a3fbbaee914b05ecf5be96
Emerson Viana <missonzito@outlook.com>
2023-02-28 21:49:59 -0300

    Gráficos de linha e ponto com matplotlib

--- projeto1.py
+++ projeto1.py
@@ -5,4 +5,9 @@ y = [1, 4, 9, 16, 25, 36]
 
 plt.plot(x,y,color='blue')
 plt.scatter(x,y,color='red')
+plt.title('Dados de demanda')
+plt.xlabel('Dia')
+plt.ylabel('#Produtos')
+plt.grid
+plt.legend(['Estimativa','Dado'])
 plt.show()
